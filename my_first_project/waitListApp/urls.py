965078fffa0e14c665890100ee18aaa1da847170
professor UIX <professor@uixlabs.in>
2023-02-10 14:47:29 +0530

    adding api for list by status :contruction:

--- my_first_project/waitListApp/urls.py
+++ my_first_project/waitListApp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import InvestmentViewSet, InvestmentUpdateAPIView, InvestmentDeleteAPIView
+from .views import InvestmentViewSet, InvestmentUpdateAPIView, InvestmentDeleteAPIView, get_available_list
 
 router = DefaultRouter()
 router.register(r'investments', InvestmentViewSet)
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('investments/<int:id>/', InvestmentUpdateAPIView.as_view(), name='investment-update'),
     path('investments/<int:id>/', InvestmentDeleteAPIView.as_view(), name='investment-delete'),
+    path('where/', get_available_list)
 ]
--- my_first_project/waitListApp/views.py
+++ my_first_project/waitListApp/views.py
@@ -1,6 +1,10 @@
 from rest_framework import viewsets, generics
+from rest_framework.response import Response
+
 from .models import Investment
 from .serializers import InvestmentSerializer
+from django.db import connection
+from django.http import HttpResponse, JsonResponse
 
 
 class InvestmentViewSet(viewsets.ModelViewSet):
@@ -19,3 +23,10 @@ class InvestmentUpdateAPIView(generics.UpdateAPIView):
 class InvestmentDeleteAPIView(generics.DestroyAPIView):
     queryset = Investment.objects.all()
     serializer_class = InvestmentSerializer
+
+
+def get_available_list(request):
+    status = request.GET.get('status')
+    raw_data = Investment.objects.raw(f'SELECT * FROM waitListApp_investment WHERE status = "{status}"')
+    data = [{field.name: getattr(obj, field.name) for field in obj._meta.fields} for obj in raw_data]
+    return JsonResponse(data, safe=False)
